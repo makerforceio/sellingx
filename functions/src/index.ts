2945a9ea9c399068bf4d8625f207979ade0459ba
Daniel Lim <7555ic@gmail.com>
2022-03-12 19:30:11 +0000

    feat: added function to maintain rolling average

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,64 @@
 import * as functions from "firebase-functions";
+// const admin = require('firebase-admin');
+import * as admin from 'firebase-admin';
+import { DocumentSnapshot } from 'firebase-admin/firestore';
+
+admin.initializeApp();
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+export const test = functions.https.onRequest(async (request, response) => {
+  const doc = await db.doc(`events/${request.query.id}`).get().then(doc => doc.data());
+  const previous_average = doc!.average_price || 0;
+  const n = doc!.n || [];
+
+  const cost = request.query.cost ? +request.query.cost : 0;
+  if (n.length >= 5) {
+    n.splice(0, 1);
+  }
+
+  n.push(cost);
+  const average_price = n.reduce((acc:number, val:number) => acc + val, 0) / n.length;
+  await db.doc(`events/${request.query.id}`)
+    .set({ n, average_price, previous_average }, { merge: true });
+
+  response.send(n);
+});
+
+const onTicketChange = async (change: functions.Change<DocumentSnapshot>, context: functions.EventContext) => {
+  functions.logger.info('stuff');
+
+  const doc = await db.doc(`events/${context.params.event}`).get().then(doc => doc.data());
+  const previous_average = doc!.average_price || 0;
+  const n = doc!.n || [];
+
+  const change_data = change.after.data();
+  if (change_data == undefined || change_data!.price == null) {
+    return;
+  }
+
+  if (n.length >= 5) {
+    n.splice(0, 1);
+  }
+
+  n.push(change_data!.price);
+  const average_price = n.reduce((acc:number, val:number) => acc + val, 0) / n.length;
+  await db.doc(`events/${context.params.event}`)
+    .set({ n, average_price, previous_average }, { merge: true });
+};
+
+// // events/{event}/tickets/{ticket}
+export const updateRollingAverageOnWrite = functions.firestore
+  .document('events/{event}/tickets/{ticket}')
+  .onWrite(onTicketChange);
+
+export const updateRollingAverageOnUpdate = functions.firestore
+  .document('events/{event}/tickets/{ticket}')
+  .onUpdate(onTicketChange);
