92cf175efc8fd7091d4c61dfe2f249a3215a3de3
Sudharshan <sudhar393@gmail.com>
2022-03-19 00:39:59 +0000

    feat: added footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,6 +8,19 @@ import Home from "./components/Home.vue";
   <div class="w-full lg:w-8/12 p-6 mx-auto">
     <Home />
   </div>
+  
+  <!-- Footer -->
+  <footer class="flex flex-col justify-center items-center text-gray-900 w-full px-4 pb-8 absolute inset-x-0 bottom-0">
+  <span class="text-sm">Designed and built with <div class="heart">❤️ </div> by 
+    <a class="underline underline-offset-1 hover:underline-offset-2 hover:text-red-500" href="https://www.google.com/">Shan</a> and 
+    <a class="underline underline-offset-1 hover:underline-offset-2 hover:text-red-500" href="https://www.google.com/">Daniel</a>.
+  </span>
+    <div class="flex flex-row my-2">
+      <a href="https://www.google.com/" class="text-xs mx-2">Terms of Use</a>
+      <a href="https://www.google.con/" class="text-xs mx-2">Privacy Policy</a>
+    </div>
+  </footer>
 </template>
 
-<style></style>
+<style>
+</style>
