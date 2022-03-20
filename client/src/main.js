7d3a928740269e8dd7c27ff49e4fdad709c3fa16
Sudharshan <sudhar393@gmail.com>
2022-03-20 12:15:06 +0000

    feat: added new firebase configs

--- client/src/main.js
+++ client/src/main.js
@@ -9,12 +9,12 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyDz4BJR-83Bu-s2uyFFUByh36n6nMegrVc",
   authDomain: "sellingx-a6131.firebaseapp.com",
-  databaseURL:
-    "https://sellingx-a6131-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://sellingx-a6131-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "sellingx-a6131",
   storageBucket: "sellingx-a6131.appspot.com",
   messagingSenderId: "1061875950744",
   appId: "1:1061875950744:web:e8bee349e8cc8706955ced",
+  measurementId: "G-YJK6XXWL3Z"
 };
 
 // Initialize Firebase
